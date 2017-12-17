18da1294256a90e34b6219fab5d9983f9e6d6d91
rofi.moslem <rofi.moslem@gmail.com>
2017-12-17 16:03:53 +0700

    Checkpoint

--- uas/models.py
+++ uas/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+class HeartRate(models.Model):
+    unit_id =  models.IntegerField(null=True)
+    heart_rate = models.IntegerField(null=True)
+    timestamp = models.DateTimeField(null=True)
+    sync_time = models.DateTimeField(null=True)
+    status = models.CharField(max_length=16, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.unit_id)
+
+class RestingHeartRate(models.Model):
+    unit_id = models.IntegerField(null=True)
+    rest_heart_rate = models.IntegerField(null=True)
+    timestamp = models.DateTimeField(null=True)
+    sync_time = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.unit_id)
+
+class StepCalories(models.Model):
+    unit_id = models.IntegerField(null=True)
+    start_timestamp = models.DateTimeField(null=True)
+    stop_timestamp = models.DateTimeField(null=True)
+    step = models.IntegerField(null=True)
+    total_calories = models.IntegerField(null=True)
+    sync_time = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return str(self.unit_id)
+
+class Stress(models.Model):
+    unit_id = models.IntegerField(null=True)
+    stress = models.IntegerField(null=True)
+    timestamp = models.DateTimeField(null=True)
+    sync_time = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.unit_id)
+
+class RawActivity(models.Model):
+    unit_id = models.IntegerField(null=True)
+    activity = models.CharField(max_length=32, null=True)
+    intensity = models.FloatField(null=True)
+    start_timestamp = models.DateTimeField(null=True)
+    stop_timestamp = models.DateTimeField(null=True)
+    duration = models.FloatField(null=True)
+    sync_time = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return str(self.unit_id)
--- uas/views.py
+++ uas/views.py
@@ -1,3 +1,84 @@
 from django.shortcuts import render
+from .models import *
+from django.db.models import Avg, Max, Min
+from datetime import datetime, timedelta
+from django.utils import timezone
+import statistics
+
 
 # Create your views here.
+def hr_detail(request, unit_id):
+    hr_min = HeartRate.objects.filter(unit_id=unit_id).aggregate(Min('heart_rate'))
+    hr_min = hr_min['heart_rate__min']
+    hr_avg = HeartRate.objects.filter(unit_id=unit_id).aggregate(Avg('heart_rate'))
+    hr_avg = round(hr_avg['heart_rate__avg'], 2)
+    hr_max = HeartRate.objects.filter(unit_id=unit_id).aggregate(Max('heart_rate'))
+    hr_max = hr_max['heart_rate__max']
+    hrs = HeartRate.objects.filter(unit_id=unit_id)
+
+    hr_data = []
+    count = 0
+    chart = [[] for _ in range(441)]
+    for hr in hrs:
+        hr_data.append(hr.heart_rate)
+        chart[count].append(count)
+        chart[count].append(hr.heart_rate)
+        count += 1  
+    hr_median = round(statistics.median(hr_data), 2)
+    hr_mode = statistics.mode(hr_data)
+    hr_stdev = round(statistics.stdev(hr_data), 2)
+    hr_var = round(statistics.variance(hr_data), 2)
+
+    context = {
+        'unit_id': unit_id,
+        'hr_min': hr_min,
+        'hr_avg': hr_avg,
+        'hr_max': hr_max,
+        'hr_median': hr_median,
+        'hr_mode': hr_mode,
+        'hr_stdev': hr_stdev,
+        'hr_var': hr_var,
+        'chart': chart
+    }
+    return render(request, 'uas/detail.html', context)
+
+def hr_predict(unit_id, amount):
+    limit = 1
+    while limit <= amount:
+        akhir = HeartRate.objects.filter(unit_id=unit_id).aggregate(Max('timestamp'))
+        akhir = akhir['timestamp__max']
+        awal -= timedelta(hours=1)
+        count = 0
+        while awal <= akhir:
+            hr = HeartRate.objects.get(timestamp=awal)
+            hr_count += hr  
+            awal += timedelta(minutes=1)
+            count += 1
+        hr_avg = round(hr_count / count) 
+        hr = HeartRate.objects.create(unit_id=unit_id, heart_rate=hr_avg, timestamp=(akhir + timedelta(minutes=1)), sync_time=timezone.now())
+        limit += 1
+        print(limit)
+        print(hr.heart_rate)
+    
+
+def hr_status(unit_id):
+    awal = HeartRate.objects.filter(unit_id=unit_id).aggregate(Min('timestamp'))
+    awal = awal['timestamp__min']
+    akhir = HeartRate.objects.filter(unit_id=unit_id).aggregate(Max('timestamp'))
+    akhir = akhir['timestamp__max']
+    while awal <= akhir:
+        hr = HeartRate.objects.get(timestamp=awal)
+        if hr.heart_rate <= 80 and hr.heart_rate >= 60:
+            hr.status = "Normal"
+        elif hr.heart_rate < 60:
+            hr.status = "Bradycardia"
+        else:
+            hr.status = "Thacycardia"
+        hr.save  
+        awal += timedelta(minutes=1)
+        print(awal)
+        print(hr.status)
+
+
+
+
